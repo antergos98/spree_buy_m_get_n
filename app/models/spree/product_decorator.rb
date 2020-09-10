609a4f827da5184d15bcffd735581f8f6bac7762
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2020-09-10 19:26:51 -0400

    remove_method

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Product.class_eval do
   has_many :product_promotion_rules, class_name: 'Spree::ProductPromotionRule'
   has_many :promotion_rules, through: :product_promotion_rules, class_name: 'Spree::PromotionRule'
+  remove_method :has_and_belongs_to_many
 end
